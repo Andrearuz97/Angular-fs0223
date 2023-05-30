7c3671065b92984fcea3e27ac4d7b80a918aa9c0
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-05-30 16:47:52 +0200

    updates

--- Sett1/g2/exerciseDay2/src/app/app.module.ts
+++ Sett1/g2/exerciseDay2/src/app/app.module.ts
@@ -1,14 +1,45 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Route, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostAttiviComponent } from './components/post-attivi/post-attivi.component';
+import { PostNonAttiviComponent } from './components/post-non-attivi/post-non-attivi.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'post-attivi',
+    component: PostAttiviComponent,
+  },
+  {
+    path: 'post-non-attivi',
+    component: PostNonAttiviComponent,
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ButtonsComponent,
+    HomeComponent,
+    PostAttiviComponent,
+    PostNonAttiviComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
