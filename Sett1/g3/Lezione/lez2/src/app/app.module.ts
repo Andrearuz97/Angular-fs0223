fd30da318ef4013f05eeb4bc864a00f439c43bd1
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-05-31 10:23:17 +0200

    updates

--- Sett1/g3/Lezione/lez2/src/app/app.module.ts
+++ Sett1/g3/Lezione/lez2/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ChildComponent } from './components/child/child.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ChildComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule
