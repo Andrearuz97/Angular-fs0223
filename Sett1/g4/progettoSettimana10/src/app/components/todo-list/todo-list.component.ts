68a44760b80ac3cc20bb2c4bcef484dd16a6e042
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-06-01 17:32:25 +0200

    qualcosa...

--- Sett1/g4/progettoSettimana10/src/app/components/todo-list/todo-list.component.ts
+++ Sett1/g4/progettoSettimana10/src/app/components/todo-list/todo-list.component.ts
@@ -5,7 +5,6 @@ import { Todo } from '../../model/todo.interface';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[] = [];
